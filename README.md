1d907c53cdf759e5fa7abe3f13960965b9396e18
Nathan Mota <nathansmota@gmail.com>
2023-12-07 16:50:05 -0300

    :wrench: Project setup

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # landing-page-insove-medical-healthcare
+
+Landing Page utilizando Tailwind CSS.
+
+[TailwindCSS](https://tailwindcss.com/)
+[Remix Icons](https://www.jsdelivr.com/package/npm/remixicon/)
+[Swiper](https://swiperjs.com/)
+[Scroll Reveal](https://scrollrevealjs.org/)
---
+++ tailwind.config.js
@@ -0,0 +1,44 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    container: {
+      padding: {
+        DEFAULT: '15px',
+      }
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '960px',
+      xl: '1330px',
+    },
+    extend: {
+      colors: {
+        primary: '#242a2b',
+        secondary: '#808080',
+        accent: {
+          DEFAULT: '#1cbccf',
+          secondary: '#18abbc',
+          tertiary: '#90c6cb',
+        },
+        grey: '#e8f0f1',
+      },
+      fontFamily: {
+        primary: 'Poppins, sans-serif',
+      },
+      boxShadow: {
+        custom1: '0px 2px 40px 0px rgba(8, 70, 78, 0.08)',
+        custom2: '0px 0x 30px 0px rgba(8, 73, 81, 0.06)',
+      },
+      backgroundImage: {
+        'services': 'url("/assets/images/services-bg.png")',
+        'testimonials': 'url("/assets/images/testimonials-bg.png")',
+        'departments': 'url("/assets/images/departments-bg.png")',
+        'quoteLeft': 'url("/assets/images/quote-left.png")',
+        'quoteRight': 'url("/assets/images/quote-right.png")',
+      },
+    },
+  },
+  plugins: [],
+};
