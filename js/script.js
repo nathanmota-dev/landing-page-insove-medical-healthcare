b3534aa09dd5026952719d709045e8ceb2f3bb72
Nathan Mota <nathansmota@gmail.com>
2023-12-13 16:54:18 -0300

    :lipstick: feat: Create Depoiments and Common questions

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Mobile menu
 const mobileNav = document.querySelector('.mnav');
 const closeBtn = document.querySelector('.mnav__close-btn');
 const closeBtnIcon = document.querySelector('.mnav__close-btn-icon');
@@ -19,4 +20,37 @@ closeBtn.addEventListener('click', () => {
         closeBtnIcon.classList.remove(arrowRightClass);
         closeBtnIcon.classList.add(arrowLeftClass);
     }
+});
+
+//swiper.js - depoiments
+const swiper = new Swiper('.swiper', {
+
+    loop: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+});
+
+//FAQ
+const faqItems = document.querySelectorAll('.faqItem');
+
+faqItems.forEach((item) => {
+    const faqBtn = item.querySelector('.faqBtn');
+    const faqText = item.querySelector('.faqText');
+
+    item.addEventListener('click', () => {
+        const isOpen = item.classList.toggle('open');
+        const iconClass = isOpen ? 'ri-subtract-fill' : 'ri-add-fill';
+        const iconElement = faqBtn.querySelector('i');
+        iconElement.classList = `${iconClass} text-2xl`;
+
+        if (isOpen) {
+            faqText.style.height = `${faqText.scrollHeight}px`;
+        } else {
+            faqText.style.height = '0px';
+        }
+    });
 });
